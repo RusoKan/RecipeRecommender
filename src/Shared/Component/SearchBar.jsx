dab2b9c4f0b090dad553687ec29992b72e6ee2af
RusoKan <rusosingam@gmail.com>
2023-06-24 06:31:26 -0400

    AutoFiller for the searchbar is Working
        -Initial letter makes an api call to recipe website to obtain all relevent data
        -User can type number of character which will filter the possible options
        -Have to refactor CSS code

---
+++ src/Shared/Component/SearchBar.jsx
@@ -0,0 +1,90 @@
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Card from '../Wrapper/Card';
+import Button from '../FormElement/Button';
+import { useState } from 'react';
+import axios from "axios"
+import "./SearchBar.css"
+var Meallist=[] 
+function SearchBar(params) {
+ const[MealList,setMealList]=useState([])
+  const[currentSearch,setCurrentSearch]=useState("")
+  const[searchOptions,setSearchOptions]=useState(false)
+    function handleClickSearch(event) {
+        
+        setCurrentSearch(event.target.innerHTML)
+        setSearchOptions(false)
+    }
+
+    function handleChange(event) {
+        console.log("letter",event.target.value)
+        setCurrentSearch(event.target.value)
+        setSearchOptions(true)
+        if(event.target.value.length===1 && event.target.value.match(/[a-z]/i))
+        {
+            Meallist=[]
+            axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${event.target.value}`)
+            .then((response)=>
+            {
+                for (let index = 0; index < response.data.meals.length; index++) {
+
+                    Meallist.push(response.data.meals[index].strMeal)
+                     
+                }
+                setMealList([Meallist][0])
+                
+                // 
+                
+            // Meallist=response.data.meals.length
+            })
+        }
+        else if(event.target.value.length>1 && event.target.value.match(/[a-z]/i)){
+            let newMealList= []
+            for (let index = 0; index < Meallist.length; index++) {
+                if( Meallist[index].slice(0,event.target.value.length).toLowerCase()===event.target.value.toLowerCase()){
+                    newMealList.push(Meallist[index])
+                }
+                
+            }
+
+            console.log("2letter",Meallist[0].slice(0,event.target.value.length))
+            setMealList([newMealList][0])
+        }
+        else{
+            setMealList([])
+        }
+        
+    }
+
+    return  <Card>
+       <Form>
+           <InputGroup size="lg">
+                
+                <Form.Control
+                  aria-label="Large"
+                  aria-describedby="inputGroup-sizing-sm"
+                  
+                  value={currentSearch}
+                  onChange={handleChange}
+                />
+                <Button style="primary">Search</Button>
+              </InputGroup>
+       </Form>
+       <div className={`${searchOptions &&'searchBarContainer'}`}>
+           
+           { searchOptions && MealList.map((value, index) => {  
+         return (
+            
+                <ul 
+                className='searchBarLine' 
+                key={index}><li onClick={handleClickSearch} >{value}</li>
+                </ul>
+    
+         )
+       
+     
+})}
+</div>
+   </Card>
+}
+export default SearchBar
