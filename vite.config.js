50f7230d8ea3786228cc803019881759626f44f4
RusoKan <rusosingam@gmail.com>
2023-08-25 11:48:43 -0400

    Verifying that client side connects with server side

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://recipecourtserver.nn.r.appspot.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
