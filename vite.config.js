18e5e7802fc19a04832eaf4215d03615dda7de94
RusoKan <rusosingam@gmail.com>
2023-08-31 07:31:57 -0400

    Bug fixed , Website is deployed
            -fixed it by using netlify platform instead of Vercel

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://recipecourtserver.nn.r.appspot.com',
+        target: 'https://recipecourtserver.nn.r.appspot.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
